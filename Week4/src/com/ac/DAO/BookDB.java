a64e59a5e4c7f8856111bdcb410cb27fe9b401a0
shubham42124 <68896449+shubham42124@users.noreply.github.com>
2022-02-25 16:27:44 +0530

    Update BookDB.java

--- Week4/src/com/ac/DAO/BookDB.java
+++ Week4/src/com/ac/DAO/BookDB.java
@@ -1,18 +1,20 @@
 package com.ac.DAO;
-
+// Import Packages
 import java.sql.Connection;
 import java.sql.DriverManager;
-
+import java.sql.SQLException
+//class bookDb
 public class BookDB 
 {
 
 	public static Connection getConnection() throws Exception
 		{
-		
+		         //Loading Driver Class
 			String DriverName = "org.postgresql.Driver";
 			Class.forName(DriverName);
 			
-			Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost/BookInfo","root","root"+"");
+		       //Execute Connection
+			Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost/shubham","postgres","shubham"+"");
 			
 			return conn;
 		
