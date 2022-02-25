fc965d543500f87e82648c67bee273b15881ea68
shubham42124 <68896449+shubham42124@users.noreply.github.com>
2022-02-25 16:01:19 +0530

    Create Book.java

---
+++ Week4/src/com/ac/bean/Book.java
@@ -0,0 +1,83 @@
+package com.ac.bean;
+
+public class Book {
+
+	private int bookId;
+	private String bookTitle;
+	private float bookPrice;
+	private String grade;
+	
+	
+	
+	
+	
+	public int getBookId() {
+		return bookId;
+	}
+
+
+
+
+
+	public void setBookId(int bookId) {
+		this.bookId = bookId;
+	}
+
+
+
+
+
+	public String getBookTitle() {
+		return bookTitle;
+	}
+
+
+
+
+
+	public void setBookTitle(String bookTitle) {
+		this.bookTitle = bookTitle;
+	}
+
+
+
+
+
+	public float getBookPrice() {
+		return bookPrice;
+	}
+
+
+
+
+
+	public void setBookPrice(int bookPrice) {
+		this.bookPrice = bookPrice;
+	}
+
+
+
+
+
+	public String getGrade() {
+		return grade;
+	}
+
+
+
+
+
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+
+
+
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
