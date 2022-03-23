b4714be96fa206919b01055327ca1c41d96bee82
shubham42124 <68896449+shubham42124@users.noreply.github.com>
2022-03-23 19:23:45 +0530

    Update Test.java

--- JavaProject/Covid-Info/src/main/java/com/demo/Test.java
+++ JavaProject/Covid-Info/src/main/java/com/demo/Test.java
@@ -1,52 +1,32 @@
+package com.demo;
+
 
    
-<%@page import="java.sql.DriverManager"%>
-<%@page import="java.sql.ResultSet"%>
-<%@page import="java.sql.Statement"%>
-<%@page import="java.sql.Connection"%>
 
-<%
-String driverName = "org.postgresql.Driver";
-String connectionUrl = "jdbc:postgresql://localhost:5432/students";
-String userId = "postgres";
-String password = "shubham";
-try {
-Class.forName(driverName);
-} catch (ClassNotFoundException e) {
-e.printStackTrace();
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class Test {
+
+	@RequestMapping("/sub")
+	public ModelAndView add(HttpServletRequest request, HttpServletResponse response) {
+
+		ModelAndView mv = new ModelAndView();
+
+		mv.setViewName("display.jsp");
+
+		return mv;
+
+	}
+
 }
-Connection connection = null;
-Statement statement = null;
-ResultSet resultSet = null;
-%>
-<h2 align="center"><font><strong>Fetched Covid Information</strong></font></h2>
-<table align="center" cellpadding="5" cellspacing="5" border="1">
-<tr>
-
-</tr>
-<tr bgcolor="#A52A2A">
-<td><b>Location</b></td>
-<td><b>Total Cases</b></td>
-</tr>
-<%
-try{ 
-connection = DriverManager.getConnection(connectionUrl, userId, password);
-statement=connection.createStatement();
-String sql ="SELECT location, total_cases FROM coviddata WHERE new_cases<300 GROUP BY location,total_cases,new_cases ORDER BY new_cases;";
-resultSet = statement.executeQuery(sql);
-while(resultSet.next()){
-%>
-<tr bgcolor="#DEB887">
-
-<td><%=resultSet.getString("location") %></td>
-<td><%=resultSet.getString("total_cases") %></td>
-
-</tr>
-
-<% 
-}
-} catch (Exception e) {
-e.printStackTrace();
-}
-%>
-</table>
