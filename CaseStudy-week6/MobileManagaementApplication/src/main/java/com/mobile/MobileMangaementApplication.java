6a71c48c50a63ddb6748e71eb3024e6b1f205206
shubham42124 <68896449+shubham42124@users.noreply.github.com>
2022-03-25 16:54:37 +0530

    Add files via upload

---
+++ CaseStudy-week6/MobileManagaementApplication/src/main/java/com/mobile/MobileMangaementApplication.java
@@ -0,0 +1,13 @@
+package com.mobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileMangaementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileMangaementApplication.class, args);
+	}
+
+}
